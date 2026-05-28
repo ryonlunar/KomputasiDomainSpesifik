38412c4b3fe028b74fa05c1b0755b3867dfdb48b
ryonlunar <adhimas.bimo@gmail.com>
2026-05-28 09:20:45 +0700

    chore(model) : remove previous model

--- SimuCell-Allosteric/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .enzyme import hill_equation, michaelis_menten, feedback_inhibition, feedback_activation
-from .glycolysis import HexokinaseModel
-from .krebs import CitrateSynthaseModel, ATPSynthaseModel
